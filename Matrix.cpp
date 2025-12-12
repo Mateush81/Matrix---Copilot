e998e6f3d7065185528e1c98d66ddc48fb6b99e7
Mateusz Tokarz <mateusztokarz81@gmail.com>
2025-12-12 15:28:17 +0100

    pliki

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,9 +1,1 @@
-﻿// Matrix.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+﻿
---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Matrix.cpp : Ten plik zawiera funkcjê „main”. W nim rozpoczyna siê i koñczy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
